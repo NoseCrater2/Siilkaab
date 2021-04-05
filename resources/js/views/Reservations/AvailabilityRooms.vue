31b24c08370973238157c14b6a610b399e8d3106
Angel <anarqueabrn@gmail.com>
2021-04-05 14:55:39 -0500

    Vista en Grid y en Lista de Habitaciones

--- resources/js/views/Reservations/AvailabilityRooms.vue
+++ resources/js/views/Reservations/AvailabilityRooms.vue
@@ -1,13 +1,6 @@
 <template>
     <div>
       <v-card   flat>
-          <v-card-actions style="background-color: #F0F2F5">
-              <v-btn-toggle v-model="selectedview" mandatory borderless>
-                  <v-btn icon ><v-icon>mdi-view-list</v-icon></v-btn>
-                  <v-btn icon><v-icon>mdi-view-grid</v-icon></v-btn>
-              </v-btn-toggle>
-              <v-spacer></v-spacer>
-          </v-card-actions>
           <v-tabs   dark background-color="primary" left v-model="tab">
               <v-tab  v-for="(room, index) in aRooms" :key="index">
                   Habitación {{index}}
@@ -15,8 +8,9 @@
           </v-tabs>
           <v-tabs-items  v-model="tab">
               <v-tab-item eager v-for="(room, index) in aRooms" :key="index">
-                <ListAvailabilityRoom v-if="room" :key="index" :index="parseInt(index)" :room="room"/>
-          <!-- <GridAvailabilityRoom v-if="selectedview == 1" :room="room"/> -->
+                 <GridAvailabilityRoom v-if="$vuetify.breakpoint.smAndDown" :key="index" :index="parseInt(index)" :room="room"/>
+                <ListAvailabilityRoom  v-else :key="index" :index="parseInt(index)" :room="room"/>
+               
               </v-tab-item>
           </v-tabs-items>
       </v-card>
