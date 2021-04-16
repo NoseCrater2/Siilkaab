d704cd210ddf0593e67879a1744c78f5195226be
Angel <anarqueabrn@gmail.com>
2021-04-15 19:56:24 -0500

    Correción de errores y vista de reservaciones

--- app/Http/Controllers/Api/RoomAvailabilityController.php
+++ app/Http/Controllers/Api/RoomAvailabilityController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Rate;
+use DateTime;
+use App\Room;
 use App\Hotel;
+use DatePeriod;
 use App\Messages;
+use DateInterval;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-use  App\Http\Resources\RoomIndexResource;
+use App\Http\Resources\AvailabilityRoomResource;
+use App\Http\Resources\RateIndexResource;
 
 class RoomAvailabilityController extends Controller
 {
@@ -18,12 +26,6 @@ class RoomAvailabilityController extends Controller
     //  * @param  \Illuminate\Http\Request  $request
     //  * @return \Illuminate\Http\Response
     //  */
-    // public function __invoke(Hotel $hotel, Request $request)
-    // {
-           
-
-               
-    // }
 
     public function getAvailabilityRooms(Hotel $hotel, Request $request)
     {
@@ -38,18 +40,12 @@ class RoomAvailabilityController extends Controller
             return response($validator->errors(),422);
         }else{
             $results = new Collection();
-
-            if($hotel->rooms()->has('rates')->count() > 0){
-
-            }else{
-
-                $omittedIds = [];
-                $index = 0;
-                $disponibilities = new Collection();
+            $omittedIds = [];
+            $index = 0;
+            $disponibilities = new Collection();
                 
-               foreach ($data['rooms'] as $key => $room) {
-
-                $result =  RoomIndexResource::collection(
+            foreach ($data['rooms'] as $key => $room) {
+                $result =  AvailabilityRoomResource::collection(
                     $hotel->rooms()
                     ->where('quantity', '>=', 1)
                     ->where('max_adults','>=', intval($room['adults']))
@@ -57,9 +53,9 @@ class RoomAvailabilityController extends Controller
                     ->whereNotIn('id',$omittedIds)
                     ->get()
                 );
-                
-                if( isset($result[0])){
-                   $index++;
+            
+                if( isset($result[0]) ){
+                    $index++;
                     if($disponibilities->has($result[0]->id)){
                         $disponibilities[$result[0]->id] = $disponibilities[$result[0]->id] - 1 ;
                     }else{
@@ -70,17 +66,35 @@ class RoomAvailabilityController extends Controller
                     array_push($omittedIds, $disponibilities->search(0));
                     $disponibilities->forget($disponibilities->search(0));
 
-                   
+
                     $results->put($index, $result);
                 }
-
-               }
-
-
-
-            };
+            }
             return  response($results,200);
         }
+    }
+
+    public function searchRates($id, $from, $to)
+    {
+       
+        $room = Room::find($id);
+        $toAdded = Carbon::create($to)->addDays(1)->format('Y-m-d');
+        $period = new DatePeriod(new DateTime($from), new DateInterval('P1D'), new DateTime($toAdded));
+        foreach ($period as $date) {
+           $rates[]= RateIndexResource::collection($room->rates()
+                ->where('day', $date)
+                ->orWhere(function ($query) use ($date){
+                    $query->where('start', '<=', $date)
+                          ->where('end', '>=', $date);
+                })
+                ->orWhere(lcfirst($date->format('l')),'>',0)
+                ->get()
+            );
+        }
+
+        return $rates;
+       
+
     }
 
 }
---
+++ resources/js/components/DetailRooms/CardHotelInfo.vue
@@ -0,0 +1,29 @@
+<template>
+        <v-row>
+            <v-col cols="4">
+                <v-card height="450" flat>
+                <v-img width="216" height="161" :src="`/img/${hotel.image}`">
+                </v-img>
+                </v-card>
+              
+            </v-col >
+            <v-col cols="8">
+                <v-card height="450" flat>
+                    <v-card-text>
+                        {{hotel.large_text}}
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+</template>
+
+<script>
+import {mapState } from 'vuex';
+export default {
+    computed:{
+        ...mapState({
+            hotel : state => state.HotelModule.hotel,
+        }),
+    },
+}
+</script>
